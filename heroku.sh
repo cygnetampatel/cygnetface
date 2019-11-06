7a10a3dd3ce19e00c533e364440b261e89a1e7de
cygnetampatel <cygnet.ampatel@gmail.com>
2019-11-06 08:49:14 +0530

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,4 @@
 apt-get install g++
+apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
 echo "You must install the Heroku CLI"
 g++ --version
