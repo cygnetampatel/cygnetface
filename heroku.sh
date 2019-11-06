e9c26c755bce28cc34ddcc8fde213b889ee4a62e
cygnetampatel <cygnet.ampatel@gmail.com>
2019-11-06 17:04:48 +0530

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -1,4 +1,5 @@
 apt-get install g++
 apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libc6-dev
 apt-get install avr-libc
+apt-get install libpython-dev
 g++ --version
